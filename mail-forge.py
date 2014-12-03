0d92db42ae14dbf17af5f4ab9a8a70a0a57e5b5f
KarlheinzN <karlheinz.niebuhr@gmx.com>
2014-12-03 00:08:51 -0300

    added user input, TODO: see how to manage the smtp server options better

--- mail-forge.py
+++ mail-forge.py
@@ -1,45 +1,79 @@
-#! /usr/local/bin/python
+SMTPserver = 'aspmx.l.google.com'
+print '''
+        +=======================================+
+        |..........  Mail Forge v 1  ...........|
+        +---------------------------------------+
+        |# Author: Karl Cyberfreak              |
+        |                                       |
+        |# Date: 02/12/2014                     |
+        |# This tool is made for educational    |
+        |# purposes only                        |
+        |                                       |
+        |# I take no responsibilities for the   |
+        |  use of this program !                |
+        +=======================================+
+        |..........  Mail Forge v 1  ...........|
+        +---------------------------------------+
+'''
+print "Note: - This tool can send mails from any address"
+print "This tool uses the SMTP protocol"
+
+def read_from_user():
+    host = str(raw_input("Enter hostname of smtp server (example:google.com), leave empty to use default: ")).strip()
+    if len(host) > 0:
+        global SMTPserver
+        SMTPserver = getdns(host)
+        print 'SMTP server: '+str(SMTPserver)
 
 
-SMTPserver = 'alt4.gmail-smtp-in.l.google.com'
-sender =     'karlheinzniebuhr89@gmail.com'
-destination = ['karlheinzniebuhr89@gmail.com']
+    sender      = str(raw_input("Enter Sender address: ")).strip()
+    destination = str(raw_input("Enter Destination address : ")).strip()
+    subject     = str(raw_input("Enter Email subject: ")).strip()
+    content     = str(raw_input("Enter Email content: ")).strip()
+    return sender, destination, subject, content
 
-USERNAME = "karlheinzniebuhr89@gmail.com"
-PASSWORD = "PASSWORD_INTERNET_SERVICE_PROVIDER"
 
 # typical values for text_subtype are plain, html, xml
 text_subtype = 'plain'
 
-
-content="""\
-helo
-"""
-
-subject="Sent from Python"
-
 import sys
 import os
 import re
+import dns.resolver
 
-#from smtplib import SMTP_SSL as SMTP       # this invokes the secure SMTP protocol (port 465, uses SSL)
-from smtplib import SMTP                  # use this for standard SMTP protocol   (port 25, no encryption)
+from smtplib import SMTP
 from email.MIMEText import MIMEText
 
-try:
-    msg = MIMEText(content, text_subtype)
-    msg['Subject']=       subject
-    msg['From']   = sender # some SMTP servers will do this automatically, not all
+def getdns(host):
+    answers = dns.resolver.query(host, 'MX')
+    for rdata in answers:
+        return str(rdata.exchange).strip()
 
-    conn = SMTP(SMTPserver)
-    conn.set_debuglevel(False)
 
-    #conn.login(USERNAME, PASSWORD)
+def run():
     try:
-        conn.sendmail(sender, destination, msg.as_string())
-    finally:
-        print('done')
-        conn.close()
+        sender, destination, subject, content = 'kkk@gmail.com','karlheinzniebuhr89@gmail.com','testing','message content'
+        #sender, destination, subject, content = read_from_user()
 
-except Exception, exc:
-    sys.exit( "mail failed; %s" % str(exc) ) # give a error message
+        msg = MIMEText(content, text_subtype)
+        msg['Subject']= subject
+        msg['From']   = sender # some SMTP servers will do this automatically, not all
+
+        conn = SMTP(SMTPserver)
+        conn.set_debuglevel(True)
+        conn.ehlo()
+
+        try:
+            conn.sendmail(sender, destination, msg.as_string())
+        finally:
+            print('done')
+            conn.close()
+
+    except Exception, exc:
+        sys.exit( "mail failed; %s" % str(exc) ) # give a error message
+
+
+
+
+if __name__ == '__main__':
+    run()
